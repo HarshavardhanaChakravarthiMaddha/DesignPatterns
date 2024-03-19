36c82e32d5c18c47f5ee2707fa041e56ba426983
MADDHA HARSHAVARDHANA CHAKRAVARTHI <harshavardhana.c@apexon.com>
2024-03-19 10:55:58 +0530

    ProxyChange

--- src/ProxyPattern/Client.java
+++ src/ProxyPattern/Client.java
@@ -3,7 +3,7 @@ package ProxyPattern;
 public class Client {
     public static void main(String[] args) throws Exception {
 //        DatabaseExecuter nonAdminExecuter = new DatabaseExecuterProxy("NonAdmin", "Admin@123");
-//        nonAdminExecuter.executeDatabase("DELEE");
+//        nonAdminExecuter.executeDatabase("DELETE");
 
 
 
